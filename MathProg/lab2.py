0eb6284e14b0d970209a54cfbdf961c7e7862d43
ivan.igaikin <ivan.igaikin@simbirsoft.com>
2022-04-04 14:29:12 +0300

    MathProg. Lab 2 is ready

--- MathProg/lab2.py
+++ MathProg/lab2.py
@@ -1,17 +1,15 @@
 import numpy as np
-import numdifftools as nd
 from scipy.optimize import fmin
 
 #задаём функцию
 #f = lambda x_vec: 2 * x_vec[0]**2 + x_vec[0] * x_vec[1] + x_vec[1]**2
 f = lambda x_vec: 4 * (x_vec[0] - 5)**2 + (x_vec[1] - 6)**2
 
-ZERO = 1e-4 #"условный ноль" для использования при сравнении вещественных чисел  с нулем
+ZERO = 1e-1 #"условный ноль" для использования при сравнении вещественных чисел  с нулем
 
 dim = 2
 d_array = np.eye(dim) #массив, содержащий направления
-n = len(d_array) - 1
-d0 = d_array[n]
+d0 = d_array[dim - 1]
 
 #метод для нахождения tk
 def find_tk(yi, di):
@@ -20,7 +18,32 @@ def find_tk(yi, di):
 
     return fmin(fi, t0, disp=False)[0]
 
-x0 = np.array([8.0, 9.0]) #начальное приближение
+def get_di(i):
+    if i == 0:
+        return d0
+    elif i == dim - 1:
+        return d_array[0]
+    elif i == dim:
+        return d_array[dim - 1]
+    else:
+        return d_array[i]
+
+def set_yi_in_y_array(yi, i, k):
+    if i >= len(y) or k == 0:
+        y.append(yi)
+    else:
+        y[i] = yi
+
+def update_d_array():
+        for i in range(dim - 1):
+            d_array[i] = d_array[i + 1]
+
+        #массивы в Pyhton хранятся в виде столбцов, что требуется учесть для дальнейших расчётов
+        d_array = d_array.transpose()
+
+#начальное приближение
+#x0 = np.array([0.5, 1.0])
+x0 = np.array([8.0, 9.0]) 
 eps1, eps2 = 0.1, 0.15 #задаваемая точность
 M = 10 #предельное число итераций
 
@@ -31,19 +54,20 @@ y = [x0]
 while k < M:
     is_founded = False
     i = 0
-    while i < dim:
-        ti = find_tk(y[i], d_array[i])
-        y.append(y[i] + ti * d_array[i]) #находим y[i + 1]
-        if i == n - 1:
-            if np.all(y[n] == y[0]):
-                xk = y[n]
+    while i <= dim:
+        di = get_di(i)
+        ti = find_tk(y[i], di)
+        set_yi_in_y_array(y[i] + ti * di, i, k) #находим y[i + 1]
+        if i == dim - 2: #i = n - 1
+            if np.all(y[dim - 1] == y[0]):
+                xk = y[dim - 1]
                 is_founded = True
                 break
             else:
                 i = i + 1
-        elif i == n:
-            if np.all(y[n + 1] == y[1]):
-                xk = y[n + 1]
+        elif i == dim - 1: # i = n
+            if np.all(y[dim] == y[1]):
+                xk = y[dim]
                 is_founded = True
                 break
             else:
@@ -51,21 +75,19 @@ while k < M:
         else:
             i = i + 1
 
-
-    print(y)
-    xk_new = np.array(y[n + 1])
+    xk_new = np.array(y[-1])
     if np.linalg.norm(xk_new - xk) < eps1: #проверка на сходимость
         break
 
     xk = xk_new
 
-    for i in range(dim - 1):
-        d_array[i] = d_array[i + 1]
+    tmp_diff = y[-1] - y[1]
+    for i in range(dim):
+        d_array[i, dim - 1] = tmp_diff[i]
+    
+    d0 = d_array[dim - 1]
 
-    d_array[n] = y[n + 1] - y[1]
-    d0 = d_array[n]
-
-    y = [xk]
+    y[0] = xk
 
     k = k + 1
   #цикл начинается сначала
